f2a7e448d35020e729e8af53c42b0e47bc43f8eb
Nickjasonsp <nickjasonsp@gmail.com>
2025-01-26 16:24:21 -0300

    Serialização e desserialização

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
-﻿using (HttpClient cliente = new HttpClient())
+﻿using ScreenSoundAPI.Modelos;
+using System.Text.Json;
+
+using (HttpClient cliente = new HttpClient())
 {
     try
     {
         string resposta = await cliente.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesDaMusica();
     }
     catch (Exception ex)
     {
         Console.WriteLine($"Temos um problema: {ex.Message}");
     }
+
+
 
 }
