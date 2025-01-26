8c757c85645480058267209364c05efea8aae6bd
Nickjasonsp <nickjasonsp@gmail.com>
2025-01-26 15:57:30 -0300

    Aprendendo API C#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using (HttpClient cliente = new HttpClient())
+{
+    try
+    {
+        string resposta = await cliente.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+
+}
